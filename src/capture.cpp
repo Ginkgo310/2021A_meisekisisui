bc78f42b0ebd08ab71504cceccb5c284affa91a8
Ginkgo310 <yooteerow@yahoo.co.jp>
2021-10-16 17:28:29 +0900

    ttt

--- src/capture.cpp
+++ src/capture.cpp
@@ -3,6 +3,7 @@
 #include <chrono>
 #include <ctime>
 #include <meisekisisui/realsenseInfo.h>
+#include <std_msgs/Float32MultiArray.h>
 #include <opencv2/aruco.hpp>
 #include <opencv2/opencv.hpp>
 #include <librealsense2/rs.hpp>
@@ -10,177 +11,181 @@
 #include <sstream>
 #include <fstream>
 
-constexpr std::size_t WIDTH = 1280;
-constexpr std::size_t HEIGHT = 720;
-constexpr double ratio = WIDTH / (double)HEIGHT;
+using namespace std;
+
+constexpr size_t WIDTH = 640;
+constexpr size_t HEIGHT = 480;
+constexpr double PAUL_L = 80;
+constexpr double PAUL_R = 560;
+constexpr double AREA_H = 120;
+constexpr double AREA_L = 170;
+constexpr double PAUL_DEPTH = 2.55;
+constexpr double DEPTH_MAX = PAUL_DEPTH + 0.5;
+constexpr double DEPTH_MIN = PAUL_DEPTH - 0.5;
+constexpr double strap[6] = {142, 212, 285, 355, 425, 495};
+constexpr double l_strap = 20;//142
+constexpr double r_strap = 660;//495
+constexpr int    Nummarkers = 3;
 
 int main(int argc, char **argv) try
 {
 
-    ros::init(argc, argv, "realsense_info");
-    ros::NodeHandle nh;
+	ros::init(argc, argv, "realsense_info");
+	ros::NodeHandle nh;
 
-    ros::Publisher ros_realsense_pub = nh.advertise<meisekisisui::realsenseInfo>("rs_msg", 1000);
-    ros::Rate loop_rate(100);
+	ros::Publisher marker0_pub = nh.advertise<meisekisisui::realsenseInfo>("marker0", 1000);
+	ros::Publisher marker1_pub = nh.advertise<meisekisisui::realsenseInfo>("marker1", 1000);
+	ros::Publisher marker2_pub = nh.advertise<meisekisisui::realsenseInfo>("marker2", 1000);
+	ros::Rate loop_rate(100);
 
-    ROS_INFO("Started control station");
+	ROS_INFO("Started control station");
 
-    meisekisisui::realsenseInfo rs_msg;
+	meisekisisui::realsenseInfo marker0;
+	meisekisisui::realsenseInfo marker1;
+	meisekisisui::realsenseInfo marker2;
+	
+	//rs2::colorizer cr;
 
-    //rs2::colorizer cr;
+	rs2::colorizer color_map;
+	rs2::config cfg;
+	cfg.enable_stream(RS2_STREAM_COLOR, WIDTH, HEIGHT, RS2_FORMAT_BGR8,60);
+	cfg.enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16, 90);
+	rs2::pipeline pipe;
+	auto profile = pipe.start(cfg);
+	auto depth_stream = profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+	auto intr = depth_stream.get_intrinsics();
 
-    rs2::config cfg;
-    cfg.enable_stream(RS2_STREAM_COLOR, WIDTH, HEIGHT, RS2_FORMAT_BGR8, 30);
-    cfg.enable_stream(RS2_STREAM_DEPTH, WIDTH, HEIGHT, RS2_FORMAT_Z16, 30);
-    rs2::pipeline pipe;
-    auto profile = pipe.start(cfg);
-    auto depth_stream = profile.get_stream(RS2_STREAM_COLOR).as<rs2::video_stream_profile>();
+	//dictionary
+	const cv::aruco::PREDEFINED_DICTIONARY_NAME dictionary_name = cv::aruco::DICT_4X4_50;
+	cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(dictionary_name);
 
-    // rs2::align align_to_depth(RS2_STREAM_DEPTH);
+	vector<int> marker_ids;
+	vector<std::vector<cv::Point2f>> marker_corners;
+	cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
 
-    auto intr = depth_stream.get_intrinsics();
+	double marker_x, marker_y, danger, s_count, send_data;
+	while(1) {
+		rs2::frameset frames = pipe.wait_for_frames();
+		rs2::align align_to_color(RS2_STREAM_COLOR);
+		auto aligned_frames = align_to_color.process(frames);
+		auto depth_map = aligned_frames.get_depth_frame();
+		auto color_map = aligned_frames.get_color_frame();
+		rs2::depth_frame dep = aligned_frames.get_depth_frame();
 
-    // dictionary生成
-    const cv::aruco::PREDEFINED_DICTIONARY_NAME dictionary_name = cv::aruco::DICT_4X4_50;
-    cv::Ptr<cv::aruco::Dictionary> dictionary = cv::aruco::getPredefinedDictionary(dictionary_name);
-    std::chrono::steady_clock::time_point previous_time = std::chrono::steady_clock::now();
+		cv::Mat detect (cv::Size(color_map.get_width(), color_map.get_height()), CV_8UC3, (void *)color_map.get_data(), cv::Mat::AUTO_STEP);
+		cv::Mat label = detect.clone();
+		cv::Mat sense = cv::Mat::zeros(detect.size(), CV_8UC3);
+		cv::Mat gray, mono, labeling, stats, centroids;
 
-   // cv::Mat cameraMatrix, distCoeffs;
-    
-    float distance_save[5] {0,0,0,0,0};
-    short count = 0;
-    short start_count = 0;
-    int numbering = 0;
-    //std::time_t 
-    std::ostringstream video_name,record_name;
-    auto real_time = std::chrono::system_clock::now(); 
-    std::time_t sys_time = std::chrono::system_clock::to_time_t(real_time);
-    
-    std::ofstream log;
-    record_name << std::ctime(&sys_time) << ".csv" << std::flush;
-    const std::string tmp_name = record_name.str();
-    const char* char_to_record_name = tmp_name.c_str();
-    log.open(char_to_record_name);
+		//detect markers
+		cv::aruco::detectMarkers(detect, dictionary, marker_corners, marker_ids,  parameters);
+		cv::aruco::drawDetectedMarkers(detect, marker_corners, marker_ids);
+	
+		marker0.captured = 0;
+		marker1.captured = 0;
+		marker2.captured = 0;
+	
+		if(marker_ids.size() > 0) {
+			for(int i = 0; i < marker_ids.size() ;i++) {
+			//	ROS_INFO("%d\n", marker_ids.at(i));
+				marker_x = 0;
+				marker_y = 0;
+				for(int j = 0; j < 4; j++) {
+					marker_x += marker_corners.at(i).at(j).x;
+					marker_y += marker_corners.at(i).at(j).y;
+				}
+				marker_x /= 4;
+				marker_y /= 4;
+				cv::circle(detect, cv::Point(marker_x, marker_y), 5, cv::Scalar(0, 200, 0), -1, -1);
+				switch(marker_ids.at(i)){
+					case 0:
+						marker0.x_distance = marker_x;
+						marker0.y_distance = marker_y;
+						marker0.captured   = 1;
+						break;
+					case 1:
+						marker1.x_distance = marker_x;
+						marker1.y_distance = marker_y;
+						marker1.captured   = 1;
+						break;
+					case 2:
+						marker2.x_distance = marker_x;
+						marker2.y_distance = marker_y;
+						marker2.captured   = 1;
+						break;
+				}		
+			}
+			double marker_depth = dep.get_distance(marker_x, marker_y);
+		}
+		cv::circle(detect, cv::Point(marker_x, marker_y), 5, cv::Scalar(155, 200, 0), -1, -1);
 
-    video_name << std::ctime(&sys_time) << ".mp4" << std::flush;
-    cv::VideoWriter::fourcc('M', 'P', '4', 'S');
-    cv::VideoWriter writer(video_name.str(),cv::VideoWriter::fourcc('M', 'P', '4', 'S'), 8.0, cv::Size(WIDTH,HEIGHT));
+		//sense objects
+		for(int cell_x = PAUL_L; cell_x <= PAUL_R; cell_x += 2) {
+			for(int cell_y = AREA_H; cell_y <= AREA_L; cell_y += 2) {
+				double depth = dep.get_distance(cell_x, cell_y);
+				if(depth < DEPTH_MAX && depth > DEPTH_MIN) {
+					cv::circle(sense, cv::Point(cell_x, cell_y), 1, cv::Scalar(255, 255, 255), -1);
+				}
+			}
+		}
+		cv::cvtColor(sense, gray, cv::COLOR_BGR2GRAY);
+		cv::threshold(gray, mono, 254, 255, cv::THRESH_BINARY);
+		cv::imshow("a", mono);
+		dilate(mono, sense, cv::Mat(), cv::Point(-1, -1), 1);
+		erode(sense, sense, cv::Mat(), cv::Point(-1, -1), 1);
+		erode(sense, sense, cv::Mat(), cv::Point(-1, -1), 1);
+		dilate(sense, sense, cv::Mat(), cv::Point(-1, -1), 1);
 
-    while (true)
-    {
-        
-        start_count = start_count + 1;
-        distance_save[0] = distance_save[1];
-        distance_save[1] = distance_save[2];
-        distance_save[2] = distance_save[3];
-        distance_save[3] = distance_save[4];
-        
-        rs2::frameset frames = pipe.wait_for_frames();
-        //rs2::depth_frame depth_point = frames.get_depth_frame;
-        rs2::align align_to_color(RS2_STREAM_COLOR);
-        auto aligned_frames = align_to_color.process(frames);
-        auto depth_map = aligned_frames.get_depth_frame();
-        auto color_map = aligned_frames.get_color_frame();
-//        auto colorized_depth = cr.colorize(depth_map);
+		//labeling
+		int nlab = cv::connectedComponentsWithStats(sense, labeling, stats, centroids);
+		//重心計算
+		int centerX[nlab];
+		int centerY[nlab];
+		for(int i = 1; i < nlab; ++i) {
+			double *param = centroids.ptr<double>(i);
+			centerX[i] = static_cast<int>(param[0]);
+			centerY[i] = static_cast<int>(param[1]);
+		}
 
-        cv::Mat color(cv::Size(color_map.get_width(), color_map.get_height()), CV_8UC3, (void *)color_map.get_data(), cv::Mat::AUTO_STEP);
-//        cv::Mat depth(cv::Size(depth_map.get_width(), depth_map.get_height()), CV_8UC3, (void *)colorized_depth.get_data(), cv::Mat::AUTO_STEP);
-        // マーカーの検出
-        std::vector<int> marker_ids;
-        std::vector<std::vector<cv::Point2f>> marker_corners;
-        cv::Ptr<cv::aruco::DetectorParameters> parameters = cv::aruco::DetectorParameters::create();
-        cv::aruco::detectMarkers(color, dictionary, marker_corners, marker_ids, parameters);
-//        std::vector< cv::Vec3d > rvecs, tvecs;
-//        cv::aruco::estimatePoseSingleMarkers(marker_corners, 0.05, cameraMatrix, distCoeffs, rvecs, tvecs);
+		int area_num = 0;
+		//座標
+		for(int i = 1; i < nlab; ++i) {
+			int *param = stats.ptr<int>(i);
+			if(param[cv::ConnectedComponentsTypes::CC_STAT_AREA] > 500) {
+				area_num++;
+				cv::circle(label, cv::Point(centerX[i], centerY[i]), 3, cv::Scalar(0, 0, 255), -1);
+				int x = param[cv::ConnectedComponentsTypes::CC_STAT_LEFT];
+				int y = param[cv::ConnectedComponentsTypes::CC_STAT_TOP];
+				int height = param[cv::ConnectedComponentsTypes::CC_STAT_HEIGHT];
+				int width = param[cv::ConnectedComponentsTypes::CC_STAT_WIDTH];
+				cv::rectangle(label, cv::Rect(x, y, width, height), cv::Scalar(0, 255, 0), 2);
+				std::stringstream num;
+				num << area_num;
+				putText(label, num.str(), cv::Point(x + 5, y + 20), cv::FONT_HERSHEY_COMPLEX, 0.7, cv::Scalar(0, 255, 255), 2);
+			}
+		}
 
-        float sum_marker_coordinate_x = 0;
-        float sum_marker_coordinate_z = 0;
-        float marker_coodinate_y = 0;
+		cv::imshow("detecter", detect);
+		cv::imshow("labeling", label);
 
-        if (marker_ids.size() > 0 && marker_ids.size() < 4)
-        {
-            if (marker_ids.at(0) == 0)
-            {
-                for (int i = 0; i < 4; i++)
-                {
-                    sum_marker_coordinate_x += marker_corners[0][i].x;
-                    sum_marker_coordinate_z += marker_corners[0][i].y;
-                }
+		marker0_pub.publish(marker0);
+		marker1_pub.publish(marker1);
+		marker2_pub.publish(marker2);	
 
-                std::chrono::steady_clock::time_point now_time = std::chrono::steady_clock::now();
-                std::chrono::milliseconds elapsed_time = std::chrono::duration_cast<std::chrono::milliseconds>(now_time - previous_time);
-                if (elapsed_time.count() > 15)
-                {
-                    float point_center_marker_x = sum_marker_coordinate_x / 4;
-                    float point_center_marker_z = sum_marker_coordinate_z / 4;
+		ROS_INFO("0x:%f 0y:%f 0c: %d 1x:%f 1y:%f 1c: %d 2x:%f 2y:%f 2c: %d",marker0.x_distance,marker0.y_distance,marker0.captured,marker1.x_distance,marker1.y_distance,marker1.captured,marker2.x_distance,marker2.y_distance,marker2.captured);
 
-                    float marker_distance = 0;
-
-                    distance_save[4] = (depth_map.get_distance(point_center_marker_x, point_center_marker_z) + depth_map.get_distance(point_center_marker_x + 1, point_center_marker_z) + depth_map.get_distance(point_center_marker_x - 1, point_center_marker_z) + depth_map.get_distance(point_center_marker_x, point_center_marker_z - 1) + depth_map.get_distance(point_center_marker_x, point_center_marker_z + 1))  / 5.0;
-                    start_count++;                    
-                    if(start_count > 8){
-                        marker_distance = (distance_save[0] + distance_save[1] + distance_save[2] + distance_save[3] + distance_save[4]) / 5;
-                        start_count = 8;
-                        float point[3] = {0, 0, 0};
-                        float pixel[2] = {0, 0};
-                        pixel[0] = point_center_marker_x;
-                        pixel[1] = point_center_marker_z;
-                        rs2_deproject_pixel_to_point(point, &intr, pixel, marker_distance);
-                        //double distance_sum = marker_distance + distance_sum;
-                        previous_time = now_time;
-                        float center_marker_x = point[0] * 1000; 
-                        float center_marker_y = point[2] * 1000; 
-                        float center_marker_z = point[1] * 1000; 
-                        log << center_marker_x << "," << center_marker_y << "," << center_marker_z << std::endl;
-                        ROS_INFO("x:%f  y:%f  z:%f   d:%f", center_marker_x, center_marker_y, center_marker_z, marker_distance);
-                        rs_msg.x_distance = center_marker_x;
-                        rs_msg.y_distance = center_marker_y;
-                        rs_msg.z_distance = center_marker_z;
-                    	ROS_INFO("%5.5f",center_marker_x);
-		    	ros_realsense_pub.publish(rs_msg);
-                    }
-                }
-            }
-        }
-
-        // 検出したマーカーの描画
-//        cv::aruco::drawDetectedMarkers(color, marker_corners, marker_ids);
-       // cv::aruco::drawAxis(color, cameraMatrix, distCoeffs, rvecs, tvecs, 0.1);
-        cv::Mat line_in = color;
-        cv::line(line_in,cv::Point(340,240),cv::Point(940,240), cv::Scalar(255,100,100), 5, 16);
-                
-        cv::imshow("marker_detection", line_in);
-        writer << color;
-        int key = cv::waitKey(10); 
-        if(key == 115){
-            //cv::imwrite("data.png", color);
-            std::cout << "cap" << std::endl;
-            numbering++;
-            std::ostringstream picture_name;
-            picture_name << "data" << numbering  << ".png "<< std::flush;
-            cv::imwrite(picture_name.str(), color);
-        }else if (key == 27){
-            break;
-        }   
-        
-        count = count + 1;
-        if(count > 5){
-            count = 0;
-        }
-
-        loop_rate.sleep();
-        ros::spinOnce();
-    }
-    log.close();
-    return 0;
+		if(cv::waitKey(1) == 'q') {
+			cout << "finish!!" << endl;
+			break;
+		}
+	}
 }
-catch (const rs2::error &e)
-{
-    std::cerr << "Realsense error calling" << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
-    return EXIT_FAILURE;
+catch (const rs2::error &e) {
+	cerr << "Realsense error calling" << e.get_failed_function() << "(" << e.get_failed_args() << "):\n    " << e.what() << std::endl;
+	return EXIT_FAILURE;
 }
-catch (const std::exception &e)
-{
-    std::cerr << e.what() << std::endl;
-    return EXIT_FAILURE;
+catch (const std::exception &e) {
+	cerr << e.what() << endl;
+	return EXIT_FAILURE;
 }
+
--- src/supershooter.cpp
+++ src/supershooter.cpp
@@ -1,15 +1,56 @@
 #include <ros/ros.h>
 #include <std_msgs/Int16.h>
+#include <meisekisisui/realsenseInfo.h>
+#include <std_msgs/Float32MultiArray.h>
+#include <std_msgs/Int16MultiArray.h>
 #include <iostream>
 #include <unistd.h>
 
-int move_phase = 1;
-bool shoot_admin = true;
+const float x_error  = 1.0;
+const float y_error  = 1.0;
+const float x_target = 383.0;
+const float y_target = 298.0;
+
+int move_phase = 0;
+bool shoot_admin[3] = {false,false,false};
+bool shotornot[3] = {false,false,false};
+int  returner = 0;
 
 std_msgs::Int16 shoot_ok;
 
+std_msgs::Int16 shoot_buf;
+
+float x_dist[3] = {0,0,0};
+float y_dist[3] = {0,0,0};
+bool   check[3] = {0,0,0};
+int   can_shoot = -1;
+int count = -1;
+
+void target_recognise(const std_msgs::Int16MultiArray& shoot_recogniser){
+	can_shoot = shoot_recogniser.data[0];
+	returner  = shoot_recogniser.data[1];
+}
+
 void addmission_ok(const std_msgs::Int16& yumiya_phase){
 	move_phase = yumiya_phase.data;
+}
+
+void getMarker0(const meisekisisui::realsenseInfo& marker0){	
+	x_dist[0] = marker0.x_distance;
+	y_dist[0] = marker0.y_distance;
+	check[0]  = marker0.captured;
+}
+
+void getMarker1(const meisekisisui::realsenseInfo& marker1){	
+	x_dist[1] = marker1.x_distance;
+	y_dist[1] = marker1.y_distance;
+	check[1]  = marker1.captured;
+}
+
+void getMarker2(const meisekisisui::realsenseInfo& marker2){	
+	x_dist[2] = marker2.x_distance;
+	y_dist[2] = marker2.y_distance;
+	check[2]  = marker2.captured;
 }
 
 int main(int argc,char** argv){
@@ -17,16 +58,41 @@ int main(int argc,char** argv){
 	ros::NodeHandle nh;
 	ros::Rate loop_rate(1000);
 
-	ros::Publisher yumi_pub  = nh.advertise<std_msgs::Int16>("shooter",10);
+	ros::Publisher  yumi_pub       = nh.advertise<std_msgs::Int16>("shooter",10);
+	ros::Publisher  buf_pub        = nh.advertise<std_msgs::Int16>("bufbuf" ,10);
+	ros::Subscriber target_recog   = nh.subscribe("tar_recog",10,target_recognise);
 	ros::Subscriber get_addmission = nh.subscribe("addmitter",10,addmission_ok);
+	ros::Subscriber get_marker0    = nh.subscribe("marker0"  ,10,   getMarker0);
+	ros::Subscriber get_marker1    = nh.subscribe("marker1"  ,10,   getMarker1);
+	ros::Subscriber get_marker2    = nh.subscribe("marker2"  ,10,   getMarker2);
+
 	while(ros::ok()){
-		if(shoot_admin == true && move_phase == 3){
-			shoot_ok.data = 3;
-			yumi_pub.publish(shoot_ok);
-		}else if(move_phase == 1){
-			shoot_ok.data = 1;
-			yumi_pub.publish(shoot_ok);
+		for(int i = 0; i < 3; ++i){
+			shoot_admin[i] = false;
 		}
+
+		for(int i = 0; i < 3; ++i){
+			if((x_dist[i] - x_target) < x_error && check[i] == 1){
+				if((y_dist[i] - y_target) < y_error){
+					shoot_admin[i]	= true;
+				}					
+			}
+		}
+		
+		switch(move_phase){
+			case 1:
+				shoot_ok.data = 1;
+				break;
+			case 3:
+				if(shoot_admin[can_shoot] == true){
+					shoot_ok.data  = 3;
+					shoot_buf.data = returner;
+					buf_pub.publish(shoot_buf);
+				}
+				break;
+		}
+		yumi_pub.publish(shoot_ok);
+
 		ros::spinOnce();
 		loop_rate.sleep();
 	}
